5c929b809a829396b0bf94b51a797241ff933eaf
Anirudh Suryawanshi <91246782+Anirudh-108@users.noreply.github.com>
2025-04-01 12:15:22 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+// Creating User Schema
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    image: { type: String, required: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Creating User Model
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
